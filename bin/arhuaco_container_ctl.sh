b9d3a1db0886b7dfa019922659bc588ce14d2210
Andres Gomez <kurono@riseup.net>
2019-09-14 11:08:27 +0000

    adds a script to execute the arhuaco analysis service inside a docker container. refs #23

---
+++ bin/arhuaco_container_ctl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" == "start" ]
+then
+    echo "Starting Arhuaco..."
+    sudo docker run --name parsec-arhuaco -it -d \
+                    -v /var/lib/arhuaco/data:/var/lib/arhuaco/data \
+                    -v /var/log/arhuaco:/var/log/arhuaco \
+                    -v /var/log/bro/current:/var/log/bro/current \
+                    arhuaco /bin/bash
+elif [ "$1" == "stop" ]
+then
+    echo "Killing Arhuaco..."
+    sudo docker stop parsec-arhuaco
+else
+    echo "Usage: arhuaco_container_ctl.sh <start/stop"
+fi
