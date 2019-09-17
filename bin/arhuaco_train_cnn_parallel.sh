2cf7be8bbe2415d75ab9c21adb85ad8b193f4742
Andres Gomez <kurono@riseup.net>
2019-09-17 15:51:09 +0000

    improves the parallel training of arhuaco. refs #24.

--- bin/arhuaco_train_cnn_parallel.sh
+++ bin/arhuaco_train_cnn_parallel.sh
@@ -3,4 +3,4 @@
 PYTHON_COMMAND="python3"
 ARHUACO_LOCATION="/usr/local/lib/python3.5/dist-packages/arhuaco/"
 
-nohup $PYTHON_COMMAND "$ARHUACO_LOCATION"/training/train_cnn_w2v.py -t $1 </dev/null >/var/log/arhuaco/output.log 2>&1 &
+nohup $PYTHON_COMMAND "$ARHUACO_LOCATION"/training/train_cnn_w2v.py -t $1 2>> /var/log/arhuaco/training-arhuaco.log 2>&1 &
