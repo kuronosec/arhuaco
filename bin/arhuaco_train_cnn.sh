5934e613d7557dac03c5bea401357e658c9646a2
Andres Gomez <kurono@riseup.net>
2019-09-17 15:23:44 +0000

    adds minor changes to the training procedure. refs #24.

--- bin/arhuaco_train_cnn.sh
+++ bin/arhuaco_train_cnn.sh
@@ -3,4 +3,4 @@
 PYTHON_COMMAND="python3"
 ARHUACO_LOCATION="/usr/local/lib/python3.5/dist-packages/arhuaco/"
 
-$PYTHON_COMMAND "$ARHUACO_LOCATION"/training/train_cnn_w2v.py -t $1 2> /var/log/arhuaco/arhuaco.log
+$PYTHON_COMMAND "$ARHUACO_LOCATION"/training/train_cnn_w2v.py -t $1 2>> /var/log/arhuaco/training-arhuaco.log
